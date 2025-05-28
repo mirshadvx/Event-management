cd17809b41f3663a0184e2370ffb94bc46ad61b5
mirshad <mirshadkvx@gmail.com>
2025-05-28 10:10:57 +0530

    feat(server): Dockerized the backend server, created related files to do the Dockerization

---
+++ server/entrypoint.sh
@@ -0,0 +1,12 @@
+echo "Waiting for PostgreSQL..."
+
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+exec "$@"
--- server/event_management/settings.py
+++ server/event_management/settings.py
@@ -3,26 +3,18 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from pathlib import Path
 from decouple import config
-load_dotenv()
 from celery.schedules import crontab
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'daphne',
@@ -58,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 REST_FRAMEWORK = {
@@ -100,34 +92,21 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("redis", 6379)],
         },
     },
 }
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'evenxo',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv("DB_HOST"),
         'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -144,10 +123,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -156,29 +131,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": "redis://127.0.0.1:6379/1",
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#         }
-#     }
-# }
-
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -188,7 +144,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
@@ -196,53 +151,44 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
-    'LEEWAY': 100,  # Allows 10 seconds of clock difference    
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),  # Set token expiry time
+    'LEEWAY': 100,
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
-    "ROTATE_REFRESH_TOKENS": True,  
+    "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "ALGORITHM": "HS256",
-    "SIGNING_KEY": SECRET_KEY,  # Use your Django secret key
-    "AUTH_HEADER_TYPES": ("Bearer",),  # Still allows Authorization header
-    "AUTH_COOKIE": "access_token",  # Name of the cookie
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",), 
+    "AUTH_COOKIE": "access_token",
     "AUTH_COOKIE_REFRESH": "refresh_token",
-    "AUTH_COOKIE_SECURE": False,  # Set to False for local development
+    "AUTH_COOKIE_SECURE": False,
     "AUTH_COOKIE_HTTP_ONLY": True,
     "AUTH_COOKIE_PATH": "/",
     "AUTH_COOKIE_SAMESITE": "Lax",
-
 }
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"")
 
-# CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dzju4glap',
-    'API_KEY': '277546951167852',
-    'API_SECRET': 'bN0kXAMCKGWnxJlH3HTQnkl6wHw',
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
 import cloudinary
 
 cloudinary.config(
-    cloud_name='dzju4glap',
-    api_key='277546951167852',
-    api_secret='bN0kXAMCKGWnxJlH3HTQnkl6wHw'
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
 )
-# print("Cloudinary config:", cloudinary.config().cloud_name)
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-# CELERY_BROKER_URL = 'redis://172.29.107.116:6379/0'
-# CELERY_RESULT_BACKEND = 'redis://172.29.107.116:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 
-# CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'  # Use 127.0.0.1 instead of localhost
-# CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
-# CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
@@ -252,7 +198,6 @@ CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 REDIS_HOST = config('REDIS_HOST', default='localhost')
 REDIS_PORT = config('REDIS_PORT', default=6379, cast=int)
 REDIS_DB = config('REDIS_DB', default=0, cast=int)
-
 
 LOGGING = {
     "version": 1,
@@ -265,12 +210,6 @@ LOGGING = {
             "formatter": "verbose",
         },
     },
-    # "loggers": {
-    #     "": {
-    #         "handlers": ["console", "file"],
-    #         "level": os.environ.get("DJANGO_LOG_LEVEL", "INFO"),
-    #     }
-    # },
     "formatters": {
         "verbose": {
             "format": "{asctime} ({levelname})- {name}- {message}",
@@ -279,14 +218,6 @@ LOGGING = {
     },
 }
 
-# CELERY_BEAT_SCHEDULE = {
-#     'distribute-revenue-every-minute': {
-#         'task': 'users.tasks.distribute_event_revenue',
-#         'schedule': crontab(minute='*/2'),
-#     },
-# }
-
-# this works the every midnight to call the task distribute_event_revenue function
 CELERY_BEAT_SCHEDULE = {
     'distribute-revenue-every-midnight': {
         'task': 'users.tasks.distribute_event_revenue',
@@ -297,5 +228,3 @@ CELERY_BEAT_SCHEDULE = {
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
-
-# lush-humor-likes-agile
