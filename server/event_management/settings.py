f12ba29fa2580f2106651af577936617c7cfac03
mirshad <mirshadkvx@gmail.com>
2026-05-15 10:31:25 +0530

    fix: database render deploy

--- server/event_management/settings.py
+++ server/event_management/settings.py
@@ -92,13 +92,19 @@ TEMPLATES = [
     },
 ]
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.getenv("DATABASE_URL")
-    )
-}
-
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 REDIS_URL = os.getenv("REDIS_URL")
 
