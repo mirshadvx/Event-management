27b533fa5d1a9ddb8e1b98692922669614e8f336
mirshad <mirshadkvx@gmail.com>
2026-07-18 07:48:05 +0530

    feat: crated gatekeeper model

--- server/gatekeeper/models.py
+++ server/gatekeeper/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ManagedUser(models.Model):
+    event = models.ForeignKey(
+        "event.Event", on_delete=models.CASCADE,related_name="managed_users"
+    )
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=130)
+    permission_given_date = models.DateTimeField(default=timezone.now)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.event.event_title})"
